20ffcf960549dace5e86b58a7e1a699ea975d965
rocoide <roodrigo4@hotmail.com>
2024-10-27 16:17:11 -0300

    se uso el template Login Activity views

---
+++ app/src/main/java/com/mano_solidaria/app/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.mano_solidaria.app.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
