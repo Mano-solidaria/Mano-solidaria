8d5a66e9193256629ee4d3814be2d27a6566b9e3
Paiz <Francopaiz100@gmail.com>
2024-11-19 23:55:50 -0300

    Para el rompe bolas de roco que necesita avanzar 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
